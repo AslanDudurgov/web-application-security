4fd63cdfd33f117da40894e13d7a26d7810442d7
Aslan Dudurgov <131179573+AslanDudurgov@users.noreply.github.com>
2024-03-15 18:49:41 +0400

    practice_3

--- practice_3/Подверженное/register_vulnerable.php
+++ practice_3/Подверженное/register_vulnerable.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "practice_2");
+$mysqli = new mysqli("localhost", "root", "", "practice_3");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
